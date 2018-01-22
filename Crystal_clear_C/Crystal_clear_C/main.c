934b97748773ac599ef002a57d9191af29f5a187
KetVit <ketvitketvit@gmail.com>
2018-01-22 16:23:21 +0200

    Our witchcraft!

---
+++ Crystal_clear_C/Crystal_clear_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Crystal_clear_C
+//
+//  Created by ket on 1/22/18.
+//  Copyright © 2018 ket. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Crystal clear C!\n");
+    return 0;
+}
